bf458e1a078e250d7c42a6dec20e27dc025e73c2
Rajanrj99 <rajr0201147@gmail.com>
2024-05-21 22:21:16 +0530

    updata databse

--- myblog/settings.py
+++ myblog/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
