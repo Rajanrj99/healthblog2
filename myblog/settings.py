e09f1ec5ba351675a55d174ed5cc5c0921cbb8ec
Rajan Raj <111189057+Rajanrj99@users.noreply.github.com>
2024-05-24 13:48:25 +0530

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -81,11 +81,11 @@ import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+    # 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
